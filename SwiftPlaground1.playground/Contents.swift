0a2237ec72a2981ed927dbe61c0c5e537289a077
Schappy808 <59860894+Schappy808@users.noreply.github.com>
2020-01-21 10:37:53 -1000

    Added Variables

--- SwiftPlaground1.playground/Contents.swift
+++ SwiftPlaground1.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//var myFirstVariable: String
+
+var myFirstVariable = "I've created my first variable"
+
+//declare and assign variable in single line of code
+var mySecondVariable = "and look i added two strings together"
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
+
+var myForthVariable = "one of three"
+
+var myFifthVariable = "two of three"
+
+var mySixthVariable = "three of three"
+
+var mySeventhVariable = myForthVariable + myFifthVariable + mySixthVariable
+
+print(mySeventhVariable)
+
+
